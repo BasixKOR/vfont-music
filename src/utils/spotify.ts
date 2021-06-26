574113af21cc654ee21fd469492e64efa0e6c9b3
Basix <basix@basix.tech>
2021-06-27 04:08:28 +0900

    Fixup spotify utils

--- src/utils/spotify.ts
+++ src/utils/spotify.ts
@@ -4,11 +4,24 @@ export function getSpotifyLoginUrl(token: string): string {
   const redirect_uri = `${process.env.NEXT_PUBLIC_VERCEL_URL}/api/auth/callback`;
 
   const params = qs.stringify({
+    response_type: 'code',
     client_id: process.env.NEXT_PUBLIC_SPOTIFY_CLIENT_ID,
     redirect_uri,
-    state: 'token',
+    state: token,
     scope: 'user-read-playback-state'
   })
 
   return `https://accounts.spotify.com/authorize?${params}`;
+}
+
+export interface SpotifyAuthResponse {
+  access_token: string;
+  token_type: 'Bearer';
+  scope: string;
+  expires_in: number;
+  refresh_token: string;
+}
+
+export function isSpotifyAuthResponse(obj: unknown): obj is SpotifyAuthResponse {
+  return typeof obj === 'object' && obj !== null && 'access_token' in obj; // Unsound for sure, but okay for now.
 }
